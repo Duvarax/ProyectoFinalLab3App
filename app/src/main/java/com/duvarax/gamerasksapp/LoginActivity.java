d0e17c489f7000686a481dc7097eb0da3b4f9512
Duvara Claudio <duvaraclaudiojavier@gmail.com>
2023-06-08 01:15:00 -0300

    menu

--- app/src/main/java/com/duvarax/gamerasksapp/LoginActivity.java
+++ app/src/main/java/com/duvarax/gamerasksapp/LoginActivity.java
@@ -3,8 +3,11 @@ package com.duvarax.gamerasksapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 import com.duvarax.gamerasksapp.databinding.ActivityMainBinding;
 
@@ -16,9 +19,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-
+        setContentView(binding.getRoot());
         binding.imageView2.setImageResource(R.drawable.joystick_icon2);
         binding.imageView2.setColorFilter(ContextCompat.getColor(this, R.color.yellowMain));
-        setContentView(binding.getRoot());
+
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
