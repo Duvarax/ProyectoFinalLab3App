f9e50186ab776149b10049961623af955683e798
Duvara Claudio <duvaraclaudiojavier@gmail.com>
2023-06-09 01:20:49 -0300

    Slider

--- app/src/main/java/com/duvarax/gamerasksapp/ui/Perfil/PerfilFragment.java
+++ app/src/main/java/com/duvarax/gamerasksapp/ui/Perfil/PerfilFragment.java
@@ -1,6 +1,7 @@
 package com.duvarax.gamerasksapp.ui.Perfil;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +11,20 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.duvarax.gamerasksapp.MenuActivity;
 import com.duvarax.gamerasksapp.R;
 import com.duvarax.gamerasksapp.databinding.FragmentPerfilBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PerfilFragment extends Fragment {
 
     private FragmentPerfilBinding binding;
+    private Handler sliderHandler = new Handler();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,9 +34,42 @@ public class PerfilFragment extends Fragment {
         binding = FragmentPerfilBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         binding.imageView3.setImageResource(R.drawable.palanca_icon);
+        ArrayList<String> listaImagenes = new ArrayList<>();
+        listaImagenes.add("https://generacionxbox.com/wp-content/uploads/2021/04/epic-games-store-juegos-gratis.jpg");
+        listaImagenes.add("https://img.freepik.com/vector-gratis/conjunto-oscuro-elementos-boton-piedra-juego-barra-progreso-brillantes-diferentes-formas-botones-juegos-aplicaciones_1150-39977.jpg");
+        listaImagenes.add("https://i.pinimg.com/originals/3a/a4/ea/3aa4eaef2581fcfd78b824410bf9d61e.jpg");
+        PerfilSliderAdapter adapter = new PerfilSliderAdapter(root.getContext(), listaImagenes, getLayoutInflater(), binding.carousel, getActivity());
+        binding.carousel.setAdapter(adapter);
+
+        binding.carousel.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderCambio);
+                sliderHandler.postDelayed(sliderCambio, 3000);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
+
+
         return root;
     }
 
+    private Runnable sliderCambio = new Runnable() {
+        @Override
+        public void run() {
+            binding.carousel.setCurrentItem(binding.carousel.getCurrentItem() + 1);
+        }
+    };
     @Override
     public void onDestroyView() {
         super.onDestroyView();
---
+++ app/src/main/java/com/duvarax/gamerasksapp/ui/Perfil/PerfilSliderAdapter.java
@@ -0,0 +1,81 @@
+package com.duvarax.gamerasksapp.ui.Perfil;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.bumptech.glide.Glide;
+import com.duvarax.gamerasksapp.R;
+
+import java.util.List;
+
+public class PerfilSliderAdapter extends RecyclerView.Adapter<PerfilSliderAdapter.ViewHolder> {
+
+    private Context context;
+    private List<String> listaImagenes;
+    private LayoutInflater inflater;
+    private ViewPager2 viewpager2;
+    private Activity activity;
+
+    public PerfilSliderAdapter(Context context, List<String> listaImagenes, LayoutInflater inflater, ViewPager2 viewpager2, Activity activity) {
+        this.context = context;
+        this.listaImagenes = listaImagenes;
+        this.inflater = inflater;
+        this.viewpager2 = viewpager2;
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public PerfilSliderAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context =parent.getContext();
+        View root = inflater.inflate(R.layout.slider_item, parent, false);
+        return new ViewHolder(root);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PerfilSliderAdapter.ViewHolder holder, int position) {
+        Glide.with(context)
+                .load(listaImagenes.get(position))
+                .into(holder.imagen);
+        holder.imagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(activity,R.id.nav_host_fragment_activity_menu).navigate(R.id.navigation_home);
+            }
+        });
+        if (position == listaImagenes.size() - 2){
+            viewpager2.post(runnable);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaImagenes.size();
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            listaImagenes.addAll(listaImagenes);
+            notifyDataSetChanged();
+        }
+    };
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imagen;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imagen = itemView.findViewById(R.id.ivSliderFoto);
+        }
+    }
+}
